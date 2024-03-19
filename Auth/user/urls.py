1c3f3a5e79387511b159ecc1f46cca763c4c85db
unknown <Garaxq@gO6xDIGh4T.gO6xDIGh4T>
2024-03-19 17:52:00 +0200

    garaxq

---
+++ Auth/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('user/', UserView.as_view()),
+    path('logout/', LogoutView.as_view()),
+]
